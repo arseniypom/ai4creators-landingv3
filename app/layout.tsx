55a46b5820b44aea3f05d9fc0dcfbc9bb6e02031
arseniypom <arpomazkov@bk.ru>
2026-01-25 12:43:40 +0000

    Updated favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,6 +38,13 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: [
+      { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: "/apple-icon.png",
+  },
 };
 
 export default function RootLayout({
